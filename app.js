1bba3c9a54c524ff39c8df376cb645db42a36e52
Alexander Torrijos <artorrijos@gmail.com>
2019-07-28 14:02:13 +0800

    Local temp changes

--- app.js
+++ app.js
@@ -1,102 +1,106 @@
 // Reference to the root application directory
 // for cleaner require calls in other files
-// Usage: 
+// Usage:
 // var Schools = require(`{__base}/models/Schools`)
 // instead of
 // var Schools = require("../../../../models/Schools");
 global.__base = __dirname;
 
 const express = require('express'),
-path = require('path'),
-bodyParser = require('body-parser'),
-session = require('express-session'),
-cors = require('cors'),
-errorHandler = require('errorhandler'),
-mongoose = require('mongoose'),
-responseTime= require('response-time');
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  cors = require('cors'),
+  errorHandler = require('errorhandler'),
+  mongoose = require('mongoose'),
+  responseTime = require('response-time');
 
 var swaggerJSDoc = require('swagger-jsdoc'),
-expressPaginate = require('express-paginate');
-   
+  expressPaginate = require('express-paginate');
 
 mongoose.promise = global.Promise;
 
 const isProduction = process.env.NODE_ENV === 'production',
-app = express();
+  app = express();
 
 var swaggerDefinition = {
-    info: {
-        title: 'School Rankings API',
-        version: '1.0.0',
-        description: 'Integrating RESTful API with Swagger',
-        termsOfService: '',
-        host: '',
-        basePath: '/',
-    },
-    host: 'localhost:8000',
-    basePath: '/',
+  info: {
+    title: 'School Rankings API',
+    version: '1.0.0',
+    description: 'Integrating RESTful API with Swagger',
+    termsOfService: '',
+    host: '',
+    basePath: '/'
+  },
+  host: 'localhost:8000',
+  basePath: '/'
 };
 
 var options = {
-    swaggerDefinition: swaggerDefinition,
-    apis: ['./routes/*.js'],
+  swaggerDefinition: swaggerDefinition,
+  apis: ['./routes/*.js']
 };
 
 var swaggerSpec = swaggerJSDoc(options);
 
 app.use(cors());
 app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'school-rankings', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false}));
+app.use(
+  session({
+    secret: 'school-rankings',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
 app.use(expressPaginate.middleware(5, 5));
 app.use(responseTime());
 
-if(!isProduction){
-    app.use(errorHandler());
+if (!isProduction) {
+  app.use(errorHandler());
 }
 
-mongoose.connect("mongodb://srankings:srankings123@ds145750.mlab.com:45750/school-rankings", {useNewUrlParser: true});
+mongoose.connect(
+  'mongodb://srankings:srankings123@ds145750.mlab.com:45750/school-rankings',
+  { useNewUrlParser: true }
+);
 mongoose.set('debug', true);
-
-
 
 require('./models/Users');
 require('./models/Schools');
 require('./models/PrePrimarySchools');
-require('./config/passport');   
+require('./config/passport');
 app.use(require('./routes'));
 
-
-
-if(!isProduction){
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500).json({
-            errors: {
-                message: err.message,
-                error: err,
-            },
-        });
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      errors: {
+        message: err.message,
+        error: err
+      }
     });
+  });
 }
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        errors: {
-            message: err.message,
-            error: {}, 
-        },
-    });
+  res.status(err.status || 500).json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
 });
 
-app.use('/api-docs',express.static(path.join(__dirname,'api-docs')));
+app.use('/api-docs', express.static(path.join(__dirname, 'api-docs')));
 
-app.get('/swagger.json', function(req, res){
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
 });
-    app.listen(8000, () => {
-        console.log('Server running on http://localhost:8000/');
-    });
-
+app.listen(8000, () => {
+  console.log('Server running on http://localhost:8000/');
+});
--- routes/api/v1/controllers/users.controller.js
+++ routes/api/v1/controllers/users.controller.js
@@ -3,83 +3,92 @@ const passport = require('passport');
 const Users = mongoose.model('Users');
 
 exports.createUser = (req, res, next) => {
-console.log();
-    const { body: {user}} = req;  
-    if(!user.username) {
-      return res.status(422).json({
-        errors: {
-          username: 'is required',
-        },
-      });
-    }
-  
-    if(!user.password) {
-      return res.status(422).json({
-        errors: {
-          password: 'is required',
-        },
-      });
-    }
-  
-  const finalUser = new Users(user);
-    return finalUser.save()
-      .then(() => res.json({ user: finalUser.toAuthJSON() }));
+  console.log(req);
+
+  const {
+    body: { user }
+  } = req;
+  console.log('yo', body, user);
+  if (!user.username) {
+    return res.status(422).json({
+      errors: {
+        username: 'is required'
+      }
+    });
   }
 
+  if (!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required'
+      }
+    });
+  }
 
-  exports.createUserLogin = (req, res, next) => {
-    const { body: { user } } = req;
-  
-    if(!user.email) {
-      return res.status(422).json({
-        errors: {
-          email: 'is required',
-        },
-      });
-    }
-  
-    if(!user.password) {
-      return res.status(422).json({
-        errors: {
-          password: 'is required',
-        },
-      });
-    }
-  
-    return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
-      if(err) {
+  const finalUser = new Users(user);
+  return finalUser
+    .save()
+    .then(() => res.json({ user: finalUser.toAuthJSON() }));
+};
+
+exports.createUserLogin = (req, res, next) => {
+  const {
+    body: { user }
+  } = req;
+
+  if (!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required'
+      }
+    });
+  }
+
+  if (!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required'
+      }
+    });
+  }
+
+  return passport.authenticate(
+    'local',
+    { session: false },
+    (err, passportUser, info) => {
+      if (err) {
         return next(err);
       }
-  
-      if(passportUser) {
+
+      if (passportUser) {
         const user = passportUser;
         user.token = passportUser.generateJWT();
         return res.json({ user: user.toAuthJSON() });
       }
-  
+
       return status(400).info;
-    });
-  }
+    }
+  );
+};
 
-  exports.getCurrentUser = (req, res, next) => {
+exports.getCurrentUser = (req, res, next) => {
+  const {
+    payload: { id }
+  } = req;
 
-    const { payload: { id } } = req;
-  
-    return Users.findById(id)
-      .then((user) => {
-        if(!user) {
-          return res.sendStatus(400);
-        }
-        return res.json({ user: user.toAuthJSON() });
-      });
-  }
+  return Users.findById(id).then(user => {
+    if (!user) {
+      return res.sendStatus(400);
+    }
+    return res.json({ user: user.toAuthJSON() });
+  });
+};
 
-  exports.getAllUsers = (req, res, next) => {
-    return Users.find()
-      .then((user) => {
-        if(!user) {
-          return res.sendStatus(400);
-        }
-        return res.json(user);
-      });
-  }
+exports.getAllUsers = (req, res, next) => {
+  return Users.find().then(user => {
+    if (!user) {
+      return res.sendStatus(400);
+    }
+    return res.json(user);
+  });
+};
